59d36ff82ec32be67c25a51ad77ca5e0ccc28227
jackzouyan <jackzoushao@gmail.com>
2020-06-09 19:17:35 +0800

    comment test

--- src/main/java/com/example/controller/DemoController.java
+++ src/main/java/com/example/controller/DemoController.java
@@ -1,7 +1,5 @@
 package com.example.controller;
 
-import com.example.service.DemoService;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- src/test/java/com/example/demo/ControllerTest.java
+++ src/test/java/com/example/demo/ControllerTest.java
@@ -1,17 +1,14 @@
 package com.example.demo;
 
 import com.example.controller.DemoController;
-import com.example.service.DemoService;
 import mockit.Expectations;
 import mockit.Injectable;
-import mockit.Mocked;
 import mockit.Tested;
 import mockit.integration.junit4.JMockit;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
+
 @RunWith(JMockit.class)
 public class ControllerTest {
 
